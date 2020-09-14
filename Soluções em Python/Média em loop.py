72d27c60e44dc2fdd7e257461c50933454af5492
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-09-14 19:05:28 -0300

    essa versão tem erro

---
+++ Soluções em Python/Média em loop.py
@@ -0,0 +1,9 @@
+print()
+loop = int(input('Informe o tamanho do vetor: '))
+print()
+vetor = []
+for i in range (1, loop):
+    vetor += int(input('Informe o {}º valor: '.format(i)))
+    print()
+media = vetor / i
+print('A média do vetor é igual a: {:.2f}.'.format(media))
