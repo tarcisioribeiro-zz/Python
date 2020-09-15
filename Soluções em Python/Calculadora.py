3957f60e3463a25816b5df9ff386d92a1e781e6d
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-09-15 20:45:23 -0300

    Adicionei a multiplicação

--- Soluções em Python/Calculadora.py
+++ Soluções em Python/Calculadora.py
@@ -20,4 +20,9 @@ while opc != 0:
         print()
         print('Resultado: {} + {} = {}.'.format(n1, n2, (n1 - n2)))
         print()
-    
+    if(opc == 1):
+        n1 = int(input('Informe o primeiro número: '))
+        n2 = int(input('Informe o segundo número: '))
+        print()
+        print('Resultado: {} + {} = {}.'.format(n1, n2, (n1 * n2)))
+        print()
