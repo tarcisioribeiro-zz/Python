1adfa9c9ba836fa4b1fdff36588c78b64822546e
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-17 21:44:26 -0300

    Exercício 38 feito

--- Curso de Python/Mundo 2/Aula 12/Exercícios/ex036.py
+++ Curso de Python/Mundo 2/Aula 12/Exercícios/ex036.py
@@ -1,9 +1,10 @@
-#Empréstimo bancário
+# Empréstimo bancário
 nome = str(input('Oi, bom dia! Qual o seu nome? '))
 print()
 salario = int(input('{}, me diga qual o valor do seu salário: '.format(nome)))
 print()
-valorcasa = int(input('Ok, {}. Qual o valor do imóvel que deseja comprar? '.format(nome)))
+valorcasa = int(
+    input('Ok, {}. Qual o valor do imóvel que deseja comprar? '.format(nome)))
 print()
 tempo = int(input('Em quanto tempo deseja financiar o imóvel? '))
 print()
