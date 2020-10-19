b4ee22d851f9fc429149288a2b108f09c4951497
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-19 17:13:12 -0300

    Bomba finalizada

--- Soluções em Python/Soluções Diversas/Bomba.py
+++ Soluções em Python/Soluções Diversas/Bomba.py
@@ -6,12 +6,13 @@ print()
 print(emoji.emojize('Desarme a bomba! :bomb:', use_aliases=True))
 print()
 fios = ['Amarelo', 'Azul', 'Vermelho']
-escolha = str(input(emoji.emojize('Qual fio você vai cortar?\n\nAmarelo\nAzul\nVermelho\n\nCorra! Digite aqui o nome da cor do fio: ', use_aliases=True)))
+escolha = str(input(emoji.emojize(
+    'Qual fio você vai cortar?\n\nAmarelo\nAzul\nVermelho\n\nCorra! Digite aqui o nome da cor do fio: ', use_aliases=True)))
 print()
 if escolha == 'Azul':
-    print('Ufa! Você desarmou a bomba.')
+    print('Ufa! Você desarmou a bomba!')
 else:
-    for i in range(11, 0, -1):
+    for i in range(10, 0, -1):
         print('{}!'.format(i))
         sleep(1)
     print()
