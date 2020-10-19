cad6d2fd6b118c25769777f458b58e9230109f33
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-18 21:19:11 -0300

    Melhorei o exercício 44.

--- Curso de Python/Mundo 2/Aula 12/Exercícios/ex044.py
+++ Curso de Python/Mundo 2/Aula 12/Exercícios/ex044.py
@@ -1,9 +1,18 @@
 # Juros de valor pago sobre um produto
+
+# Vamos melhorar esse exercício!!!
+
+# Ok, acho que está bom assim!!
+from time import sleep
+import emoji
+import time
+print()
 produto = float(input('Informe o valor do produto: '))
 print()
-pagamento = str(input('Selecione o método de pagamento:\n\nA - À vista (dinheiro/cheque)\nB - À vista - cartão\nC - 2 vezes no cartão\nD - 3 ou mais vezes no cartão\n\nInforme sua opção: '))
+sleep(1)
+pagamento = str(input(emoji.emojize('Selecione o método de pagamento:\n\nA - À vista (dinheiro/cheque) :dollar: \nB - À vista - cartão :credit_card: \nC - 2 vezes no cartão :credit_card: \nD - 3 ou mais vezes no cartão :credit_card: \n\nInforme sua opção: ', use_aliases=True)))
 print()
-
+sleep(1)
 if(pagamento == 'A'):
     print('O valor do pagamento é de R$ {:.2f}.'.format(produto*0.9))
     print()
