b93483514e071d51c8a3978d0872bd2000fe2f8e
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-22 12:21:55 -0300

    Adicionando o escopo das fases

--- Games em Python/Jogo do Carrinho em Python/python/main.py
+++ Games em Python/Jogo do Carrinho em Python/python/main.py
@@ -4,4 +4,15 @@ import emoji
 print()
 print(emoji.emojize('Bem vindo ao jogo do carrinho! :red_car:', use_aliases=True))
 print()
-print(emoji.emojize('Fase da floresta :evergreen_tree::evergreen_tree::evergreen_tree::evergreen_tree::evergreen_tree:', use_aliases=True))
+fase = int(input(emoji.emojize(
+    'Qual fase deseja jogar? \n\n[ 1 ] Fase da floresta :evergreen_tree:\n[ 2 ] Fase da praia :palm_tree:\n[ 3 ] Fase do deserto :cactus:\n\nDigite aqui sua opção: ', use_aliases=True)))
+print()
+if(fase == 1):
+    print(emoji.emojize('Fase da floresta :evergreen_tree::evergreen_tree::evergreen_tree::evergreen_tree::evergreen_tree:', use_aliases=True))
+print()
+if(fase == 2):
+    print(emoji.emojize('Fase da praia :palm_tree::palm_tree::palm_tree::palm_tree::palm_tree:', use_aliases=True))
+print()
+if(fase == 3):
+    print(emoji.emojize('Fase do deserto :cactus::cactus::cactus::cactus::cactus:',use_aliases=True))
+print()
