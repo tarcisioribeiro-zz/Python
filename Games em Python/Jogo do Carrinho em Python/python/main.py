fd38a74cdb28cb3f30577282591a86e10a48424a
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-22 12:13:06 -0300

    Testando a impressão de emojis

--- Games em Python/Jogo do Carrinho em Python/python/main.py
+++ Games em Python/Jogo do Carrinho em Python/python/main.py
@@ -2,4 +2,6 @@ import pygame
 import time
 import emoji
 print()
-print(emoji.emojize('Bem vindo ao jogo ', use_aliases=True))
+print(emoji.emojize('Bem vindo ao jogo do carrinho! :red_car:', use_aliases=True))
+print()
+print(emoji.emojize('Fase da floresta :evergreen_tree::evergreen_tree::evergreen_tree::evergreen_tree::evergreen_tree:', use_aliases=True))
