45b59d6a5ab133c72abe2ec636b4a5978762898a
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-22 18:43:37 -0300

    Escopo Fase 2 concluído.

--- Games em Python/Jogo do Carrinho em Python/python/main.py
+++ Games em Python/Jogo do Carrinho em Python/python/main.py
@@ -4,9 +4,9 @@ import emoji
 print()
 print(emoji.emojize('Bem vindo ao jogo do carrinho! :red_car:', use_aliases=True))
 print()
-escolha = ''
 fase = int(input(emoji.emojize(
-    'Qual fase deseja jogar? \n\n[ 1 ] Fase da floresta :evergreen_tree:\n[ 2 ] Fase da praia :palm_tree:\n[ 3 ] Fase do deserto :cactus:\n\nDigite aqui sua opção: ', use_aliases=True)))
+        'Qual fase deseja jogar? \n\n[ 1 ] Fase da floresta :evergreen_tree:\n[ 2 ] Fase da praia :palm_tree:\n[ 3 ] Fase do deserto :cactus:\n\nDigite aqui sua opção: ', use_aliases=True)))
+print()
 if(fase == 1):
     print()
     print(emoji.emojize('Fase da floresta :evergreen_tree::evergreen_tree::evergreen_tree::evergreen_tree::evergreen_tree:', use_aliases=True))
@@ -44,7 +44,41 @@ if(fase == 2):
     print()
     print(emoji.emojize('Fase da praia :palm_tree::palm_tree::palm_tree::palm_tree::palm_tree:', use_aliases=True))
     print()
+    print(emoji.emojize(':palm_tree:__________:red_car:', use_aliases=True))
+    sleep(1)
+    print(emoji.emojize(':palm_tree:_________:red_car:', use_aliases=True))
+    sleep(1)
+    print(emoji.emojize(':palm_tree:________:red_car:', use_aliases=True))
+    sleep(1)
+    print(emoji.emojize(':palm_tree:_______:red_car:', use_aliases=True))
+    sleep(1)
+    print(emoji.emojize(':palm_tree:______:red_car:', use_aliases=True))
+    sleep(1)
+    print(emoji.emojize(':palm_tree:_____:red_car:', use_aliases=True))
+    sleep(1)
+    print(emoji.emojize(':palm_tree:____:red_car:', use_aliases=True))
+    sleep(1)
+    print(emoji.emojize(':palm_tree:___:red_car:', use_aliases=True))
+    sleep(1)
+    print(emoji.emojize(':palm_tree:__:red_car:', use_aliases=True))
+    sleep(1)
+    print(emoji.emojize(':palm_tree:_:red_car:', use_aliases=True))
+    sleep(1)
+    escolha = str(input('Desvie! o carrinho vai bater! Escreva desviar!\n\nEscreve aqui: '))
+    sleep(1)
+    print()
+    if(escolha == 'desviar'):
+        print(emoji.emojize('Ufa! o Carrinho desviou! \n\n:red_car:_:palm_tree:', use_aliases=True))
+        print()
+    else:
+        print(emoji.emojize(':boom::boom::boom::boom::boom:', use_aliases=True))
+        print()
 if(fase == 3):
     print()
     print(emoji.emojize('Fase do deserto :cactus::cactus::cactus::cactus::cactus:',use_aliases=True))
     print()
+else:
+    print()
+    print('Opção inválida. Jogo encerrado.')
+    print()
+    
