627fff76944ed364a621e599b4e8bf6f1e0b8e77
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-26 12:37:27 -0300

    Melhorei o exercício 64.

--- Curso de Python/Mundo 2/Aula 14/Exercícios/ex064.py
+++ Curso de Python/Mundo 2/Aula 14/Exercícios/ex064.py
@@ -3,7 +3,7 @@ n = 0
 cont = 0
 soma = 0
 while (n != 999):
-    n = int(input('Informe um número: '))
+    n = int(input('Informe um número: [999 para parar] '))
     cont += 1
     soma += n
 print()
