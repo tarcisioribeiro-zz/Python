162ff2f99a251981e0fb7a1b1b61c8e956568f90
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-02 23:13:43 -0300

    Exercício em andamento

--- Soluções em Python/Soluções Humanas/Identificação de veículo.py
+++ Soluções em Python/Soluções Humanas/Identificação de veículo.py
@@ -8,6 +8,8 @@ if(vel <= 300):
     print('É um carro.')
 elif(vel > 300 and vel <= 450):
     print('É um helicóptero.')
-elif(vel > 450):
+elif(vel > 450 and vel <= 2750):
     print('É um avião.')
+else:
+    print('Objeto muito veloz, não foi possível reconhecer.')
 print()
