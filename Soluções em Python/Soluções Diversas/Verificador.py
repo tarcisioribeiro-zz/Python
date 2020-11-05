c01d436f1147ae7619ac0f9af75b6c5b9df3c349
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-05 12:53:43 -0300

    Deletei o verificador de senha.

--- Soluções em Python/Soluções Diversas/Verificador.py
+++
@@ -1,9 +0,0 @@
-senha = str(input('Digite a senha: '))
-print()
-conf = str(input('Confirme a senha: '))
-print()
-if(senha == conf):
-    print('As senhas correspondem!')
-    print()
-else:
-    print('As senhas não correspondem.')
