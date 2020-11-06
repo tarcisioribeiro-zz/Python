165eb699dec483cbe106d9ce9d4478aab157f08c
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-05 22:40:02 -0300

    Corrigindo palavras.

--- Solucoes/Solucoes-Matematicas/Pontuacao.py
+++ Solucoes/Solucoes-Matematicas/Pontuacao.py
@@ -6,18 +6,18 @@ partida = int(
 print()
 print('Para computar os resultados, precisamos que digite o resultado.\n\nD - DERROTA\nE - EMPATE\nV - VITÓRIA\n')
 
-pontuacao = 0
+pontuação = 0
 resultado = ''
 
 for i in range(1, partida + 1):
     resultado = str(input('Informe o resultado da {}ª partida: '.format(i)))
     if(resultado == 'D'):
-        pontuacao += 0
+        pontuação += 0
     if(resultado == 'E'):
-        pontuacao += 1
+        pontuação += 1
     if(resultado == 'V'):
-        pontuacao += 3
+        pontuação += 3
 
 print()
-print('A pontuação do clube foi de {} pontos.'.format(pontuacao))
+print('A pontuação do clube foi de {} pontos.'.format(pontuação))
 print()
