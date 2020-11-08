76d888d124d44cbbb6172ae311ba967a49cdc584
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-08 17:34:44 -0300

    Fase do Japão Criada.

--- Games-em-Python/Jogo-de-Aventura/source/main.py
+++ Games-em-Python/Jogo-de-Aventura/source/main.py
@@ -1,7 +1,10 @@
+# Bloco de importações das bibliotecas
 from time import sleep
 import emoji
 import time
 import datetime
+
+# Bloco de sáida que dá as boas vindas ao usuário
 now = datetime.datetime.now()
 print()
 time.sleep(1)
@@ -15,18 +18,22 @@ print()
 print(emoji.emojize('Vamos pegar um avião! :airplane:', use_aliases=True))
 time.sleep(1)
 print()
-destino = ''
+
+# Bloco de entrada que solicita ao usuário a inserção dos seus dados
+destino = 0
 while destino != 999:
     destino = int(input(
-        'Qual o destino deseja seguir: \n\n[ 1 ] Japão\n[ 2 ] Coréia do Sul\n[ 3 ] China\n[ 4 ] Estados Unidos\n[ 5 ] França\n[ 6 ] Espanha\n[ 7 ] Itália\n[ 8 ] Rússia\n[ 9 ] Reino Unido\n[10] Alemanha\n\nDigite aqui sua opção: '))
+        'Qual o destino deseja seguir: \n\n[ 1 ] Japão\n[ 2 ] Coréia do Sul\n[ 3 ] China\n[ 4 ] Estados Unidos\n[ 5 ] França\n[ 6 ] Espanha\n[ 7 ] Itália\n[ 8 ] Rússia\n[ 9 ] Reino Unido\n[ 10 ] Alemanha\n\nDigite aqui sua opção: '))
     time.sleep(1)
     print()
+
+    # Bloco de decisão
     if(destino == 1):
         print(emoji.emojize(' :airplane: ' * 10, use_aliases=True))
         time.sleep(1)
         print()
         pegar = 0
-        print('Bem vindo ao japão!')
+        print('Bem vindo ao Japão!')
         time.sleep(1)
         print()
         while pegar != 999:
@@ -35,17 +42,41 @@ while destino != 999:
             time.sleep(1)
             print()
             if(pegar == 1):
+                print(
+                    'A distância até Tokyo é de 10 Km, você gastará 30 minutos de bicicleta.')
+                time.sleep(1)
+                print()
                 print(emoji.emojize(' :bike: ' * 30, use_aliases=True))
                 time.sleep(1)
                 print()
+                print('Parabéns, você chegou!')
+                time.sleep(1)
+                print()
+                break
             elif(pegar == 2):
+                print(
+                    'A distância até Tokyo é de 10 Km, você gastará 15 minutos de carro.')
+                time.sleep(1)
+                print()
                 print(emoji.emojize(' :car: ' * 15, use_aliases=True))
                 time.sleep(1)
                 print()
+                print('Parabéns, você chegou!')
+                time.sleep(1)
+                print()
+                break
             elif(pegar == 3):
+                print(
+                    'A distância até Tokyo é de 10 Km, você gastará 5 minutos de trem-bala.')
+                time.sleep(1)
+                print()
                 print(emoji.emojize(' :bullettrain_side: ' * 5, use_aliases=True))
                 time.sleep(1)
                 print()
+                print('Parabéns, você chegou!')
+                time.sleep(1)
+                print()
+                break
             elif(pegar == 999):
                 print('Ok! Esperamos que volte em breve!')
                 time.sleep(1)
@@ -79,6 +110,8 @@ while destino != 999:
         break
     else:
         print('Opção não reconhecida. Informe uma opção válida ou digite 999 para sair.')
+# Bloco de saída final
 print()
 time.sleep(1)
-print(emoji.emojize('Acesse meu Github :octocat:! https://github.com/tarcisioribeiro', use_aliases=True))
+print(emoji.emojize(
+    'Acesse meu Github :octocat:! https://github.com/tarcisioribeiro', use_aliases=True))
