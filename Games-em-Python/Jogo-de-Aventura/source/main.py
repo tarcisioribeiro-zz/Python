5605ab2398f21f1a2625221a0967efc5d4289fb5
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-08 15:07:01 -0300

    Adicionando o input.

--- Games-em-Python/Jogo-de-Aventura/source/main.py
+++ Games-em-Python/Jogo-de-Aventura/source/main.py
@@ -7,7 +7,18 @@ time.sleep(1)
 print(emoji.emojize('{} :earth_americas:', use_aliases=True).format(now))
 print()
 time.sleep(1)
-print(emoji.emojize('Bem vindo ao jogo da cobrinha! :snake:', use_aliases=True))
+print(emoji.emojize(
+    'Bem vindo ao jogo de aventura! :runner: :runner: :runner:', use_aliases=True))
 time.sleep(1)
 print()
+print(emoji.emojize('Vamos pegar um avião! :airplane:', use_aliases=True))
 time.sleep(1)
+print()
+destino = ''
+while destino != 'L':
+    destino = str(int(input(
+        'Qual o destino deseja seguir: \n\nJapão\n:Coréia do Sul\nChina\nEstados Unidos\nFrança\nEspanha\nItália\nRússia\nReino Unido\nAlemanha\n\nDigite aqui sua opção: ', use_aliases=True))).strip().upper()
+    time.sleep(1)
+    print()
+    if(destino == 'J'):
+        print()
