dfa7511698df5419da956fd2d7c8315926990a7c
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-11 23:46:56 -0300

    Fase da Coréia concluída.

---
+++ Games-em-Python/Jogo-de-Aventura/source/fases/coréia.py
@@ -0,0 +1,23 @@
+from source.decisões.estrutura_veículo import Estrutura_veículo
+import pygame
+import time
+import emoji
+
+def Coréia():
+    pygame.mixer.music.load('library/sounds/decolagem.mp3')
+    pygame.mixer.music.play()
+    time.sleep(8)
+    print(emoji.emojize(' :airplane: ' * 10, use_aliases=True))
+    time.sleep(1)
+    print()
+    pegar = 0
+    pygame.mixer.music.load('library/sounds/coréia.mp3')
+    pygame.mixer.music.play()
+    print('Bem vindo a Coréia!')
+    time.sleep(6)
+    print()
+    
+    Estrutura_veículo()
+
+    pass
+
