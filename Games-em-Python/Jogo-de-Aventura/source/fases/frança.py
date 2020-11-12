67076611593627ff157e00ea1b2450a327127aef
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-12 00:05:16 -0300

    Reorganizando o diretório de sons.

---
+++ Games-em-Python/Jogo-de-Aventura/source/fases/frança.py
@@ -0,0 +1,23 @@
+from source.decisões.estrutura_veículo import Estrutura_veículo
+import pygame
+import time
+import emoji
+
+def França():
+    pygame.mixer.music.load('library/sounds/decolagem.mp3')
+    pygame.mixer.music.play()
+    time.sleep(8)
+    print(emoji.emojize(' :airplane: ' * 10, use_aliases=True))
+    time.sleep(1)
+    print()
+    pegar = 0
+    pygame.mixer.music.load('library/sounds/frança.mp3')
+    pygame.mixer.music.play()
+    print('Bem vindo a França!')
+    time.sleep(7)
+    print()
+    
+    Estrutura_veículo()
+
+    pass
+
