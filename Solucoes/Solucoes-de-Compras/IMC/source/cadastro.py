32b2461457cd4528a9c7f5779316fe17fa16e0a9
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-12 16:37:39 -0300

    Método de Cadastro realizado.

--- Solucoes/Solucoes-de-Compras/IMC/source/cadastro.py
+++ Solucoes/Solucoes-de-Compras/IMC/source/cadastro.py
@@ -1,1 +1,16 @@
+from time import sleep
 
+def Cadastro():
+    nome = str(int(input('Informe seu nome: ')))
+    sleep(1)
+    print()
+    sexo = str(int(input('Informe seu sexo: '))).strip().upper()
+    sleep(1)
+    print()
+    peso = float(input('Informe seu peso: '))
+    sleep(1)
+    print()
+    altura = float(input('Informe sua altura em metros: (Para separar, utilize o .) '))
+    sleep(1)
+    print()
+    pass
