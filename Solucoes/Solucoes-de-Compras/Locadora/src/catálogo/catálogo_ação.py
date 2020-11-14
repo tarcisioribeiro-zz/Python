c549515a4d8e44a2ab3d3ddedec465d78b071021
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-14 11:23:29 -0300

    Finalizei a categoria e o catálogo de Ação.

---
+++ Solucoes/Solucoes-de-Compras/Locadora/src/catálogo/catálogo_ação.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+
+def Catálogo_Ação():
+    print()
+    sleep(1)
+    print('Rambo')
+    print('Duro de Matar')
+    print('Predador')
+    sleep(1)
+    print()
+    pass
