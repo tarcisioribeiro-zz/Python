003e54ade2f1bff66a5ff67c9cf7eaf200c56111
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-15 20:14:53 -0300

    Adicionei as opções de Ação e Animação.

--- Solucoes/Solucoes-de-Compras/Locadora/main.py
+++ Solucoes/Solucoes-de-Compras/Locadora/main.py
@@ -1,3 +1,4 @@
+from categorias.animação import Animação
 from src.categorias.ação import Ação
 from time import sleep
 import emoji
@@ -6,7 +7,7 @@ sleep(1)
 print(emoji.emojize('Seja bem vindo! :raising_hand:', use_aliases=True))
 print()
 sleep(1)
-print('-' * 30)
+print('-' * 60)
 sleep(1)
 print('')
 print('Bem vindo ao nosso catálogo de filmes!\nAqui estão as categorias:')
@@ -19,13 +20,19 @@ print('Drama')
 print('Romance')
 sleep(1)
 print()
-print('-' * 30)
+print('-' * 60)
 sleep(1)
 print()
+escolha = ''
 
-escolha = str(input('Informe sua escolha: ')).strip().upper()
-sleep(1)
-print()
+while escolha not in 'AÇÃOANIMAÇÃOCOMÉDIADOCUMENTÁRIODRAMAROMANCE':
+    escolha = str(input('Informe sua escolha: ')).upper()
+    sleep(1)
+    print()
+    if(escolha == 'AÇÃO'):
+        Ação()
+    if(escolha == 'ANIMAÇÃO'):
+        Animação()
 sleep(1)
 print('Acesse meu github! \n\nhttps://github.com/tarcisioribeiro')
 sleep(1)
