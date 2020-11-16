f94f3b9381fd2136f8aa9cf4fa74b44ffe09011d
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-15 22:09:38 -0300

    Corrigindo algumas fases.

--- Games-em-Python/Jogo-de-Aventura/source/fases/japão.py
+++ Games-em-Python/Jogo-de-Aventura/source/fases/japão.py
@@ -3,22 +3,21 @@ import pygame
 import time
 import emoji
 
+
 def Japão():
-    
     print(emoji.emojize(' :airplane: ' * 10, use_aliases=True))
     time.sleep(1)
+    print()
     pygame.mixer.music.load('library/sounds/main/decolagem.mp3')
     pygame.mixer.music.play()
-    time.sleep(10)
-    print()
+    time.sleep(8)
     pegar = 0
     pygame.mixer.music.load('library/sounds/fases/japão.mp3')
     pygame.mixer.music.play()
     print('Bem vindo ao Japão!')
     time.sleep(10)
     print()
-    
+
     Estrutura_veículo()
 
     pass
-
--- Games-em-Python/Jogo-de-Aventura/source/fases/reino_unido.py
+++ Games-em-Python/Jogo-de-Aventura/source/fases/reino_unido.py
@@ -5,17 +5,17 @@ import emoji
 
 
 def Reino_Unido():
-    time.sleep(18)
     print(emoji.emojize(' :airplane: ' * 10, use_aliases=True))
     time.sleep(1)
     print()
     pygame.mixer.music.load('library/sounds/main/decolagem.mp3')
     pygame.mixer.music.play()
+    time.sleep(8)
     pegar = 0
     pygame.mixer.music.load('library/sounds/fases/reino_unido.mp3')
     pygame.mixer.music.play()
     print('Bem vindo ao Reino Unido!')
-    time.sleep(10)
+    time.sleep(18)
     print()
 
     Estrutura_veículo()
