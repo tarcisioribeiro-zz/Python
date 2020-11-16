cc024ceb8f403d827e6f753f0a75d326a2f835cd
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-15 21:49:55 -0300

    Mudei a fase do Reino Unido.

--- Games-em-Python/Jogo-de-Aventura/source/fases/reino_unido.py
+++ Games-em-Python/Jogo-de-Aventura/source/fases/reino_unido.py
@@ -3,21 +3,21 @@ import pygame
 import time
 import emoji
 
+
 def Reino_Unido():
-    pygame.mixer.music.load('library/sounds/main/decolagem.mp3')
-    pygame.mixer.music.play()
-    time.sleep(8)
+    time.sleep(18)
     print(emoji.emojize(' :airplane: ' * 10, use_aliases=True))
     time.sleep(1)
     print()
+    pygame.mixer.music.load('library/sounds/main/decolagem.mp3')
+    pygame.mixer.music.play()
     pegar = 0
     pygame.mixer.music.load('library/sounds/fases/reino_unido.mp3')
     pygame.mixer.music.play()
     print('Bem vindo ao Reino Unido!')
     time.sleep(10)
     print()
-    
+
     Estrutura_veículo()
 
     pass
-
