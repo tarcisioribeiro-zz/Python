a0a3e2a15014224e8b5139ed3c2fd62f8e6f0a8a
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-16 02:59:29 -0300

    Mudei a fase da China.

--- Games-em-Python/Jogo-de-Aventura/source/fases/china.py
+++ Games-em-Python/Jogo-de-Aventura/source/fases/china.py
@@ -3,21 +3,21 @@ import pygame
 import time
 import emoji
 
+
 def China():
-    pygame.mixer.music.load('library/sounds/main/decolagem.mp3')
-    pygame.mixer.music.play()
-    time.sleep(8)
     print(emoji.emojize(' :airplane: ' * 10, use_aliases=True))
     time.sleep(1)
     print()
+    pygame.mixer.music.load('library/sounds/main/decolagem.mp3')
+    pygame.mixer.music.play()
+    time.sleep(8)
     pegar = 0
     pygame.mixer.music.load('library/sounds/fases/china.mp3')
     pygame.mixer.music.play()
     print('Bem vindo a China!')
     time.sleep(13)
     print()
-    
+
     Estrutura_veículo()
 
     pass
-
