9ea7db0f8944e1dd6344130e8db9efcfc62df2b9
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-15 21:49:45 -0300

    Mudei a fase do Japão.

--- Games-em-Python/Jogo-de-Aventura/source/fases/japão.py
+++ Games-em-Python/Jogo-de-Aventura/source/fases/japão.py
@@ -4,11 +4,12 @@ import time
 import emoji
 
 def Japão():
+    
+    print(emoji.emojize(' :airplane: ' * 10, use_aliases=True))
+    time.sleep(1)
     pygame.mixer.music.load('library/sounds/main/decolagem.mp3')
     pygame.mixer.music.play()
     time.sleep(10)
-    print(emoji.emojize(' :airplane: ' * 10, use_aliases=True))
-    time.sleep(1)
     print()
     pegar = 0
     pygame.mixer.music.load('library/sounds/fases/japão.mp3')
