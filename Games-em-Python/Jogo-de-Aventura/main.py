d1a3cb49c28e01bb4209420f79fcb355f49c6595
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-17 16:16:15 -0300

    Adicionei a fase da Argentina.

--- Games-em-Python/Jogo-de-Aventura/main.py
+++ Games-em-Python/Jogo-de-Aventura/main.py
@@ -11,6 +11,7 @@ from source.fases.coréia import Coréia
 from source.fases.japão import Japão
 from source.fases.brasil import Brasil
 from source.fases.méxico import México
+from source.fases.argentina import Argentina
 from source.decisões.saída import Saida
 import time
 import emoji
@@ -43,7 +44,7 @@ print()
 # Bloco de entrada que solicita ao usuário a inserção dos seus dados
 destino = 0
 while destino != 999:
-    destino = int(input(emoji.emojize('Qual o destino deseja seguir: \n\n[ 0 ] Brasil\n[ 1 ] Japão\n[ 2 ] Coréia do Sul\n[ 3 ] China\n[ 4 ] Estados Unidos\n[ 5 ] França\n[ 6 ] Espanha\n[ 7 ] Itália\n[ 8 ] Rússia\n[ 9 ] Reino Unido\n[ 10 ] Alemanha\n[ 11 ] México\n[ 999 ] Finalizar o programa\n\nDigite aqui sua opção: ', use_aliases=True)))
+    destino = int(input(emoji.emojize('Qual o destino deseja seguir: \n\n[ 0 ] Brasil\n[ 1 ] Japão\n[ 2 ] Coréia do Sul\n[ 3 ] China\n[ 4 ] Estados Unidos\n[ 5 ] França\n[ 6 ] Espanha\n[ 7 ] Itália\n[ 8 ] Rússia\n[ 9 ] Reino Unido\n[ 10 ] Alemanha\n[ 11 ] México\n[ 12 ] Argentina\n[ 999 ] Finalizar o programa\n\nDigite aqui sua opção: ', use_aliases=True)))
     time.sleep(1)
     print()
     # Bloco de decisão
@@ -71,6 +72,8 @@ while destino != 999:
         Alemanha()
     elif (destino == 11):
         México()
+    elif (destino == 12):
+        Argentina()
     elif(destino == 999):
         Saida()
     else:
