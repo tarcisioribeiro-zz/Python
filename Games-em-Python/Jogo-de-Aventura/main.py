a1f80002ea16904caf9838524991a20df26004ee
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-17 06:25:50 -0300

    Adicionei a fase do Brasil.

--- Games-em-Python/Jogo-de-Aventura/main.py
+++ Games-em-Python/Jogo-de-Aventura/main.py
@@ -9,6 +9,7 @@ from source.fases.eua import Eua
 from source.fases.china import China
 from source.fases.coréia import Coréia
 from source.fases.japão import Japão
+from source.fases.brasil import Brasil
 from source.decisões.saída import Saida
 import time
 import emoji
@@ -41,11 +42,13 @@ print()
 # Bloco de entrada que solicita ao usuário a inserção dos seus dados
 destino = 0
 while destino != 999:
-    destino = int(input(emoji.emojize('Qual o destino deseja seguir: \n\n[ 1 ] Japão\n[ 2 ] Coréia do Sul\n[ 3 ] China\n[ 4 ] Estados Unidos\n[ 5 ] França\n[ 6 ] Espanha\n[ 7 ] Itália\n[ 8 ] Rússia\n[ 9 ] Reino Unido\n[ 10 ] Alemanha\n[ 999 ] Finalizar o programa\n\nDigite aqui sua opção: ', use_aliases=True)))
+    destino = int(input(emoji.emojize('Qual o destino deseja seguir: \n\n[ 0 ] Brasil\n[ 1 ] Japão\n[ 2 ] Coréia do Sul\n[ 3 ] China\n[ 4 ] Estados Unidos\n[ 5 ] França\n[ 6 ] Espanha\n[ 7 ] Itália\n[ 8 ] Rússia\n[ 9 ] Reino Unido\n[ 10 ] Alemanha\n[ 999 ] Finalizar o programa\n\nDigite aqui sua opção: ', use_aliases=True)))
     time.sleep(1)
     print()
     # Bloco de decisão
-    if(destino == 1):
+    if(destino == 0):
+        Brasil()
+    elif(destino == 1):
         Japão()
     elif(destino == 2):
         Coréia()
