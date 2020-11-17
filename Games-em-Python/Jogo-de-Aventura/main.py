a6e16a36f1af5c8a873fb302b2ee4b0e2d3200f3
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-17 20:09:31 -0300

    Facilitando a visualização e criando módulos.

--- Games-em-Python/Jogo-de-Aventura/main.py
+++ Games-em-Python/Jogo-de-Aventura/main.py
@@ -1,23 +1,9 @@
 # Bloco de importações das bibliotecas
-from source.fases.alemanha import Alemanha
-from source.fases.reino_unido import Reino_Unido
-from source.fases.rússia import Rússia
-from source.fases.itália import Itália
-from source.fases.espanha import Espanha
-from source.fases.frança import França
-from source.fases.eua import Eua
-from source.fases.china import China
-from source.fases.coréia import Coréia
-from source.fases.japão import Japão
-from source.fases.brasil import Brasil
-from source.fases.méxico import México
-from source.fases.argentina import Argentina
-from source.decisões.saída import Saida
-import time
-import emoji
-import time
+from source.decisões.corpo_principal import Corpo_Principal
 import datetime
 import pygame
+import time
+import emoji
 
 # Começa o player
 pygame.mixer.init()
@@ -34,50 +20,9 @@ time.sleep(8)
 print(emoji.emojize(
     'Bem vindo ao jogo de aventura! :runner: :runner: :runner:', use_aliases=True))
 time.sleep(1)
-print()
-pygame.mixer.music.load('library/sounds/main/cintos_avião.mp3')
-pygame.mixer.music.play()
-print(emoji.emojize('Vamos pegar um avião! :airplane:', use_aliases=True))
-time.sleep(17)
-print()
 
-# Bloco de entrada que solicita ao usuário a inserção dos seus dados
-destino = 0
-while destino != 999:
-    destino = int(input(emoji.emojize('Qual o destino deseja seguir: \n\n[ 0 ] Brasil\n[ 1 ] Japão\n[ 2 ] Coréia do Sul\n[ 3 ] China\n[ 4 ] Estados Unidos\n[ 5 ] França\n[ 6 ] Espanha\n[ 7 ] Itália\n[ 8 ] Rússia\n[ 9 ] Reino Unido\n[ 10 ] Alemanha\n[ 11 ] México\n[ 12 ] Argentina\n[ 999 ] Finalizar o programa\n\nDigite aqui sua opção: ', use_aliases=True)))
-    time.sleep(1)
-    print()
-    # Bloco de decisão
-    if(destino == 0):
-        Brasil()
-    elif(destino == 1):
-        Japão()
-    elif(destino == 2):
-        Coréia()
-    elif(destino == 3):
-        China()
-    elif(destino == 4):
-        Eua()
-    elif(destino == 5):
-        França()
-    elif(destino == 6):
-        Espanha()
-    elif(destino == 7):
-        Itália()
-    elif(destino == 8):
-        Rússia()
-    elif(destino == 9):
-        Reino_Unido()
-    elif(destino == 10):
-        Alemanha()
-    elif (destino == 11):
-        México()
-    elif (destino == 12):
-        Argentina()
-    elif(destino == 999):
-        Saida()
-    else:
-        print('Opção não reconhecida. Informe uma opção válida ou digite 999 para sair.')
+Corpo_Principal()
+
 # Bloco de saída final
 time.sleep(1)
 print(
---
+++ Games-em-Python/Jogo-de-Aventura/source/decisões/corpo_principal.py
@@ -0,0 +1,65 @@
+from source.fases.alemanha import Alemanha
+from source.fases.reino_unido import Reino_Unido
+from source.fases.rússia import Rússia
+from source.fases.itália import Itália
+from source.fases.espanha import Espanha
+from source.fases.frança import França
+from source.fases.eua import Eua
+from source.fases.china import China
+from source.fases.coréia import Coréia
+from source.fases.japão import Japão
+from source.fases.brasil import Brasil
+from source.fases.méxico import México
+from source.fases.argentina import Argentina
+from source.decisões.saída import Saida
+import time
+import emoji
+import pygame
+
+
+def Corpo_Principal():
+    destino = 0
+    while destino != 999:
+        print()
+        pygame.mixer.music.load('library/sounds/main/cintos_avião.mp3')
+        pygame.mixer.music.play()
+        print(emoji.emojize('Vamos pegar um avião! :airplane:', use_aliases=True))
+        time.sleep(17)
+        print()
+        destino = int(input(emoji.emojize(
+            'Qual o destino deseja seguir: \n\n[ 0 ] Brasil\n[ 1 ] Japão\n[ 2 ] Coréia do Sul\n[ 3 ] China\n[ 4 ] Estados Unidos\n[ 5 ] França\n[ 6 ] Espanha\n[ 7 ] Itália\n[ 8 ] Rússia\n[ 9 ] Reino Unido\n[ 10 ] Alemanha\n[ 11 ] México\n[ 12 ] Argentina\n[ 999 ] Finalizar o programa\n\nDigite aqui sua opção: ', use_aliases=True)))
+        time.sleep(1)
+        print()
+        # Bloco de decisão
+        if(destino == 0):
+            Brasil()
+        elif(destino == 1):
+            Japão()
+        elif(destino == 2):
+            Coréia()
+        elif(destino == 3):
+            China()
+        elif(destino == 4):
+            Eua()
+        elif(destino == 5):
+            França()
+        elif(destino == 6):
+            Espanha()
+        elif(destino == 7):
+            Itália()
+        elif(destino == 8):
+            Rússia()
+        elif(destino == 9):
+            Reino_Unido()
+        elif(destino == 10):
+            Alemanha()
+        elif (destino == 11):
+            México()
+        elif (destino == 12):
+            Argentina()
+        elif(destino == 999):
+            Saida()
+        else:
+            print('Opção não reconhecida. Informe uma opção válida ou digite 999 para sair.')
+
+    pass
