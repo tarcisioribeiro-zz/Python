ea17e6c4f659d15764ca2fe2123ba87a478533c5
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-17 16:10:46 -0300

    Adicionei a fase do México.

---
+++ Games-em-Python/Jogo-de-Aventura/source/fases/méxico.py
@@ -0,0 +1,23 @@
+from source.decisões.estrutura_veículo import Estrutura_veículo
+import pygame
+import time
+import emoji
+
+
+def México():
+    print(emoji.emojize(' :airplane: ' * 10, use_aliases=True))
+    time.sleep(1)
+    print()
+    pygame.mixer.music.load('library/sounds/main/decolagem.mp3')
+    pygame.mixer.music.play()
+    time.sleep(8)
+    pegar = 0
+    pygame.mixer.music.load('library/sounds/fases/méxico.mp3')
+    pygame.mixer.music.play()
+    print('Bem vindo ao México!')
+    time.sleep(10)
+    print()
+
+    Estrutura_veículo()
+
+    pass
