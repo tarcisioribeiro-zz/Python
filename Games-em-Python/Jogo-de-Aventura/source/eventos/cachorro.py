c1fb916018bd3598ad49abd82f4d294e8de5ec6f
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-19 18:43:25 -0300

    Melhorei o evento do cachorro.

--- Games-em-Python/Jogo-de-Aventura/source/eventos/cachorro.py
+++ Games-em-Python/Jogo-de-Aventura/source/eventos/cachorro.py
@@ -1,11 +1,27 @@
 import emoji
+import time
+import pygame
 
 def Cachorro():
     decisão = ''
-    while decisão != 'FUGIR':
+    mordidas = 0
+    while decisão != 'FUGIR' and mordidas < 3:
         decisão = str(input('Digite fugir para desviar do cachorro: ')).upper()
+        time.sleep(1)
+        print()
+        time.sleep(1)
         if(decisão == 'FUGIR'):
+            time.sleep(1)
             print('Você fugiu do cachorro!')
+            time.sleep(1)
         else:
-            print(emoji.emojize('O cachorro te mordeu!' + ':dog:' * 10, use_aliases=True))
+            time.sleep(1)
+            print(emoji.emojize('O cachorro te mordeu!' + ' :dog: ' * 10, use_aliases=True))
+            time.sleep(1)
+            pygame.mixer.music.load('library/sounds/eventos/latida.mp3')
+            pygame.mixer.music.play()
+            time.sleep(6)
+            mordidas += 1
+            if(mordidas > 3):
+                print(emoji.emojize('Fim de jogo! :bomb:', use_aliases=True))
     pass
