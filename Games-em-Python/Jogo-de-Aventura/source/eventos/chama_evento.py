ed3d5a3de4c3a99d96c2bb20cf1b31eefe1119f6
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-19 12:51:05 -0300

    Chamada dos eventos criada.

--- Games-em-Python/Jogo-de-Aventura/source/eventos/chama_evento.py
+++ Games-em-Python/Jogo-de-Aventura/source/eventos/chama_evento.py
@@ -1,3 +1,15 @@
+import random
+from random import randint
+from source.eventos.cachorro import Cachorro
+from source.eventos.caminhão import Caminhão
+from source.eventos.ladrão import Ladrão
+
 def Chama_Evento():
-    
+    evento = randint(1, 3)
+    if(evento == 1):
+        Cachorro()
+    if(evento == 2):
+        Caminhão()
+    if(evento == 3):
+        Ladrão()
     pass
