59f7a3b39cfb60af74f825688e3d8f92045ce1de
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-19 12:56:48 -0300

    Evento do cachorro criado.

--- Games-em-Python/Jogo-de-Aventura/source/eventos/cachorro.py
+++ Games-em-Python/Jogo-de-Aventura/source/eventos/cachorro.py
@@ -1,3 +1,11 @@
+import emoji
+
 def Cachorro():
-    
+    decisão = ''
+    while decisão != 'FUGIR':
+        decisão = str(input('Digite fugir para desviar do cachorro: ')).upper()
+        if(decisão == 'FUGIR'):
+            print('Você fugiu do cachorro!')
+        else:
+            print(emoji.emojize('O cachorro te mordeu!' + ':dog:' * 10, use_aliases=True))
     pass
