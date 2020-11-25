c8500d5319767a9e0c810fc457cd70d571a21e7d
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-25 12:41:40 -0300

    Mudei o restante do jogo.

--- Games-em-Python/Jogo-de-Aventura/source/decisões/saída.py
+++ Games-em-Python/Jogo-de-Aventura/source/decisões/saída.py
@@ -6,7 +6,8 @@ import emoji
 def Saida():
     pygame.mixer.music.load('library/sounds/decisões/fim_aventura.mp3')
     pygame.mixer.music.play()
+    print()
     print('Finalizando aventura...')
-    time.sleep(10)
-
+    time.sleep(7)
+    print()
     pass
