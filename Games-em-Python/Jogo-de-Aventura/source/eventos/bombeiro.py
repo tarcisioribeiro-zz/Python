41814b1886012d7a3b6468f445e9e171a9c584e1
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-25 18:30:13 -0300

    Adicionei o carro de bombeiro.

---
+++ Games-em-Python/Jogo-de-Aventura/source/eventos/bombeiro.py
@@ -0,0 +1,14 @@
+import time
+import emoji
+import pygame
+
+def Bombeiro():
+    time.sleep(1)
+    print()
+    print(emoji.emojize(' :firetruck: ' * 20, use_aliases=True))
+    time.sleep(1)
+    pygame.mixer.music.load('library/sounds/eventos/bombeiro.mp3')
+    pygame.mixer.music.play()
+    time.sleep(14)
+    
+    pass
--- Games-em-Python/Jogo-de-Aventura/source/eventos/chama_evento.py
+++ Games-em-Python/Jogo-de-Aventura/source/eventos/chama_evento.py
@@ -3,14 +3,16 @@ from random import randint
 from source.eventos.cachorro import Cachorro
 from source.eventos.caminhão import Caminhão
 from source.eventos.ladrão import Ladrão
-
+from source.eventos.bombeiro import Bombeiro
 
 def Chama_Evento():
-    evento = randint(0, 2)
+    evento = randint(0, 9)
     if(evento == 0):
         Cachorro()
     if(evento == 1):
         Caminhão()
     if(evento == 2):
         Ladrão()
+    if(evento == 3):
+        Bombeiro()
     pass
