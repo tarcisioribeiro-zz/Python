02812ece0c80b09557d122a7a73e489ab379aa03
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-30 19:19:34 -0300

    Começando a criar a função do Atlético Paranaense.

---
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/atlético_paranaense.py
@@ -0,0 +1,13 @@
+import emoji
+import time
+import pygame
+
+def Atlético_Paranaense():
+    time.sleep(1)
+    pygame.mixer.music.load('library/sounds/times/atlético_paranaense.mp3')
+    pygame.mixer.music.play()
+    time.sleep(14)
+    print(emoji.emojize('Furacão!!!' + ' :hurricane: ' * 3,use_aliases=True))
+    time.sleep(1)
+    print()
+    pass
