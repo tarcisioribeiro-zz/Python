b166e8d873651389278be3582e35267a99de1307
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-01 19:27:37 -0300

    Cálculo de pontuação do Brasileirão feito.

--- Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/competições/brasileirão.py
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/competições/brasileirão.py
@@ -4,5 +4,31 @@ import emoji
 
 
 def Brasileirão():
-
+    empates = 0
+    derrotas = 0
+    vitórias = 0
+    pontuação = 0
+    resultado = ''
+    time.sleep(1)
+    print(emoji.emojize('Bem vindo ao Brasileirão!' + ' :soccer: ' * 3, use_aliases=True))
+    pygame.mixer.music.load('library/sounds/competições/brasileirão.mp3')
+    pygame.mixer.music.play()
+    time.sleep(30)
+    for i in range(1, 39):
+        resultado = str(input(
+            'Você deve informar o prognóstico da partida: \n\nDerrota\nEmpate\nVitória\n\nDigite aqui o resultado da {} partida: ')).upper()
+        time.sleep(1)
+        if(resultado == 'DERROTA'):
+            derrotas += 1
+        elif(resultado == 'EMPATE'):
+            empates += 1
+            pontuação += 1
+        elif(resultado == 'VITÓRIA'):
+            vitórias += 1
+            pontuação += 3
+        else:
+            print('Não reconheço esse prognóstico. Tente novamente.')
+            time.sleep(1)
+    print('A pontuação do seu time foi de {} pontos, com {} derrotas, {} empates e {} vitórias. O aproveitamento foi de {:.2f}%.')
+    time.sleep()
     pass
