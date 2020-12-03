2319d7a02d7c183f5b950b954995f0d2e34904fd
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-03 05:35:23 -0300

    Fazendo o time do bahia.

---
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/bahia.py
@@ -0,0 +1,14 @@
+import emoji
+import time
+import pygame
+
+
+def Bahia():
+    time.sleep(1)
+    pygame.mixer.music.load('library/sounds/times/bahia.mp3')
+    pygame.mixer.music.play()
+    time.sleep(14)
+    print('Bahia!!!')
+    time.sleep(1)
+    print()
+    pass
