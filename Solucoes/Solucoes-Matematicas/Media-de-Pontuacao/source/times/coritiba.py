1956cf59cc484d74885a41e31dfd0f84e17c4f15
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-15 13:20:03 -0300

    Adicionei o time do Coritiba.

---
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/coritiba.py
@@ -0,0 +1,14 @@
+import emoji
+import time
+import pygame
+
+
+def Coritiba():
+    time.sleep(1)
+    pygame.mixer.music.load('library/sounds/times/coritiba.mp3')
+    pygame.mixer.music.play()
+    time.sleep(25)
+    print('Coxa!!!')
+    time.sleep(1)
+    print()
+    pass
