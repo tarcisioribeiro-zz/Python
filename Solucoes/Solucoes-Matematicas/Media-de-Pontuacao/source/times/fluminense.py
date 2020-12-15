ac1134f8f9366b375c5e1b85107239553fe1a5a8
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-15 13:20:35 -0300

    Adicionei o time do Fluminense.

---
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/fluminense.py
@@ -0,0 +1,14 @@
+import emoji
+import time
+import pygame
+
+
+def Fluminense():
+    time.sleep(1)
+    pygame.mixer.music.load('library/sounds/times/fluminense.mp3')
+    pygame.mixer.music.play()
+    time.sleep(30)
+    print('Fluzão!!!')
+    time.sleep(1)
+    print()
+    pass
