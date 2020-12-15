ad42b4b61313d0bd6ee28567b297bb08b5c7ace6
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-14 22:27:38 -0300

    Adicionei o time do Ceará.

---
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/ceará.py
@@ -0,0 +1,14 @@
+import emoji
+import time
+import pygame
+
+
+def Ceará():
+    time.sleep(1)
+    pygame.mixer.music.load('library/sounds/times/ceará.mp3')
+    pygame.mixer.music.play()
+    time.sleep(13)
+    print('Vozão!!!')
+    time.sleep(1)
+    print()
+    pass
