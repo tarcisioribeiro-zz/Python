ff33f77fd5d733b32b3d80c86c409da2c391e639
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-14 22:28:04 -0300

    Adicionei o time do Bragantino.

---
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/bragantino.py
@@ -0,0 +1,14 @@
+import emoji
+import time
+import pygame
+
+
+def Bragantino():
+    time.sleep(1)
+    pygame.mixer.music.load('library/sounds/times/bragantino.mp3')
+    pygame.mixer.music.play()
+    time.sleep(31)
+    print('Bragantino!!!')
+    time.sleep(1)
+    print()
+    pass
