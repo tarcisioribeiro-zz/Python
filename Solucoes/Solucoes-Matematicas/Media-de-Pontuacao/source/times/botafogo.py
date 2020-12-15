0c03c797c709a952c5d68610cf3f04f3a4420796
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-14 22:27:18 -0300

    Adicionei o time do Botafogo.

---
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/botafogo.py
@@ -0,0 +1,14 @@
+import emoji
+import time
+import pygame
+
+
+def Botafogo():
+    time.sleep(1)
+    pygame.mixer.music.load('library/sounds/times/botafogo.mp3')
+    pygame.mixer.music.play()
+    time.sleep(14)
+    print(emoji.emojize('Fogão!!! :fire: :fire: :fire:', use_aliases=True))
+    time.sleep(1)
+    print()
+    pass
