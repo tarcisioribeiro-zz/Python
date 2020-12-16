c85926183d1e4d9402934c55548164a5a927bb81
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-16 09:46:02 -0300

    Encurtei o hino do Atlético.

--- Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/atlético.py
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/atlético.py
@@ -7,7 +7,7 @@ def Atlético():
     time.sleep(1)
     pygame.mixer.music.load('library/sounds/times/atlético.mp3')
     pygame.mixer.music.play()
-    time.sleep(30)
+    time.sleep(14)
     print(emoji.emojize('Galo!!!' + ' :chicken: ' * 3, use_aliases=True))
     time.sleep(1)
     print()
