62c6d7ba7019825d83d009139598c83b5ad41956
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-16 09:57:39 -0300

    Encurtei o hino do Corinthians.

--- Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/botafogo.py
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/botafogo.py
@@ -8,7 +8,7 @@ def Botafogo():
     pygame.mixer.music.load('library/sounds/times/botafogo.mp3')
     pygame.mixer.music.play()
     time.sleep(17)
-    print(emoji.emojize('Fogão!!! :fire: :fire: :fire:', use_aliases=True))
+    print(emoji.emojize('Fogão!!!' + ' :fire: ' * 3, use_aliases=True))
     time.sleep(1)
     print()
     pass
--- Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/corinthians.py
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/corinthians.py
@@ -7,8 +7,8 @@ def Corinthians():
     time.sleep(1)
     pygame.mixer.music.load('library/sounds/times/corinthians.mp3')
     pygame.mixer.music.play()
-    time.sleep(29)
-    print(emoji.emojize('Vai Corinthians!!! :eagle: :eagle: :eagle:',use_aliases=True))
+    time.sleep(17)
+    print(emoji.emojize('Vai Corinthians!!!' ' :eagle: ' * 3,use_aliases=True))
     time.sleep(1)
     print()
     pass
