e472b50226b5103e8cc1e9cd9ba9e5ee40d66486
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-16 09:58:21 -0300

    Encurtei o hino do Fluminense.

--- Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/fluminense.py
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/fluminense.py
@@ -7,7 +7,7 @@ def Fluminense():
     time.sleep(1)
     pygame.mixer.music.load('library/sounds/times/fluminense.mp3')
     pygame.mixer.music.play()
-    time.sleep(30)
+    time.sleep(14)
     print('Fluzão!!!')
     time.sleep(1)
     print()
