a130c9bc3f8249666a3385befe424616313b1616
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-16 10:02:27 -0300

    Corrigindo um erro na função do Corinthians.

--- Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/corinthians.py
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/corinthians.py
@@ -8,7 +8,7 @@ def Corinthians():
     pygame.mixer.music.load('library/sounds/times/corinthians.mp3')
     pygame.mixer.music.play()
     time.sleep(17)
-    print(emoji.emojize('Vai Corinthians!!!' ' :eagle: ' * 3,use_aliases=True))
+    print(emoji.emojize('Vai Corinthians!!!' + ' :eagle: ' * 3,use_aliases=True))
     time.sleep(1)
     print()
     pass
