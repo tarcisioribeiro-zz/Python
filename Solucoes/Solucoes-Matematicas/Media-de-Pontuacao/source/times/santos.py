9f90986eb4601e79afef7ab9d3506cde21a24601
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-17 16:29:03 -0300

    Adicionei o time do Santos.

---
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/santos.py
@@ -0,0 +1,14 @@
+import emoji
+import time
+import pygame
+
+
+def Santos():
+    time.sleep(1)
+    pygame.mixer.music.load('library/sounds/times/santos.mp3')
+    pygame.mixer.music.play()
+    time.sleep(14)
+    print('Fluzão!!!')
+    time.sleep(1)
+    print()
+    pass
