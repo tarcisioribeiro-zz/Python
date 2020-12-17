1303766f4de950948941992fd53567b974a219f0
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-17 11:07:00 -0300

    Adicionei o time do Goiás.

---
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/goiás.py
@@ -0,0 +1,14 @@
+import emoji
+import time
+import pygame
+
+
+def Fluminense():
+    time.sleep(1)
+    pygame.mixer.music.load('library/sounds/times/goiás.mp3')
+    pygame.mixer.music.play()
+    time.sleep(16)
+    print('Goiás!!!')
+    time.sleep(1)
+    print()
+    pass
