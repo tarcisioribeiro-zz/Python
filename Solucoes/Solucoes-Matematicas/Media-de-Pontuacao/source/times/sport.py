95849e71a6432a1923e42130e8ee5c8c2859d1fa
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-17 16:46:25 -0300

    Adicionei o time do Sport.

---
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/sport.py
@@ -0,0 +1,14 @@
+import emoji
+import time
+import pygame
+
+
+def Sport():
+    time.sleep(1)
+    pygame.mixer.music.load('library/sounds/times/sport.mp3')
+    pygame.mixer.music.play()
+    time.sleep(18)
+    print('Cazá. cazá. cazá cazá cazá!!!')
+    time.sleep(1)
+    print()
+    pass
