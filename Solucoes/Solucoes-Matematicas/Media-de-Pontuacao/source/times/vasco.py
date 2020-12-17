8280cbeb08adbba10d904fd4ae95bebdba425865
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-17 16:49:15 -0300

    Adicionei o time do Vasco.

---
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/times/vasco.py
@@ -0,0 +1,14 @@
+import emoji
+import time
+import pygame
+
+
+def Vasco():
+    time.sleep(1)
+    pygame.mixer.music.load('library/sounds/times/vasco.mp3')
+    pygame.mixer.music.play()
+    time.sleep(15)
+    print('Vascão!!!')
+    time.sleep(1)
+    print()
+    pass
