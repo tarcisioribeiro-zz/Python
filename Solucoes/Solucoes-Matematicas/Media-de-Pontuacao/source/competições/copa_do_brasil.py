eb0d227da3c971f572034676082ceb38c7bafed8
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-21 12:40:18 -0300

    Terminei a inserção de dados da Copa Do Brasil.

--- Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/competições/copa_do_brasil.py
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/competições/copa_do_brasil.py
@@ -12,4 +12,21 @@ def Copa_do_Brasil():
     fase = ''
     prêmio = 0
     fase = str(input('Informe em que fase seu time terminou a competição:\n\n1ª Fase - (Digite primeira)\n2ª Fase - (Digite segunda)\n3ª Fase - (Digite 3ª Fase)\n4ª Fase - (Digite quarta)\nOitavas - (Digite oitavas)\nQuartas - (Digite quartas)\nSemi - (Digite semi)\nFinal - (Digite final)\n\nDigite aqui a fase: ')).upper()
+    while fase == '':
+        if(fase == 'PRIMEIRA'):
+            prêmio = 1000000
+        elif(fase == 'SEGUNDA'):
+            prêmio = 2000000
+        elif(fase == 'TERCEIRA'):
+            prêmio = 3000000
+        elif(fase == 'QUARTA'):
+            prêmio = 5000000
+        elif(fase == 'OITAVAS'):
+            prêmio = 8500000
+        elif(fase == 'QUARTAS'):
+            prêmio = 15000000
+        elif(fase == 'SEMI'):
+            prêmio = 22000000
+        elif(fase == 'FINAL'):
+            prêmio = 30000000    
     pass
