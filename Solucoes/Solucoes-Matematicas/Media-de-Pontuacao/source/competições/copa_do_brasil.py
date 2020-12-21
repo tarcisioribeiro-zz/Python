c5038a8670c09dad1b55e6c3296b397de7838fed
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-12-21 11:15:25 -0300

    Comecei a estruturar a Copa do Brasil.

--- Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/competições/copa_do_brasil.py
+++ Solucoes/Solucoes-Matematicas/Media-de-Pontuacao/source/competições/copa_do_brasil.py
@@ -4,5 +4,12 @@ import emoji
 
 
 def Copa_do_Brasil():
-
+    time.sleep(1)
+    print(emoji.emojize('Bem vindo a Copa do Brasil!' +
+                        ' :soccer: ' * 3, use_aliases=True))
+    time.sleep(1)
+    print()
+    fase = ''
+    prêmio = 0
+    
     pass
