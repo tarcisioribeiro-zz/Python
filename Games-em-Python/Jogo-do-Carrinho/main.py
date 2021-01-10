5672a79a97ae2f1808b5dce84fefc5b99d537b88
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-10 15:56:04 -0300

    Implementando o Seletor.

--- Games-em-Python/Jogo-do-Carrinho/main.py
+++ Games-em-Python/Jogo-do-Carrinho/main.py
@@ -1,8 +1,10 @@
+from source.menu.seletor import Seletor
 import pygame
 from time import sleep
 import emoji
 print()
 print(emoji.emojize('Bem vindo ao jogo do carrinho! :red_car:', use_aliases=True))
+sleep(1)
 print()
-
-    
+sleep(1)
+Seletor
