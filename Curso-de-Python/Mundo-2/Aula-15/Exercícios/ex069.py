51e136eadd687b03e23c449672420fb6d85dfdbf
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-15 12:31:39 -0300

    O exercício 69 tem erros.

--- Curso-de-Python/Mundo-2/Aula-15/Exercícios/ex069.py
+++ Curso-de-Python/Mundo-2/Aula-15/Exercícios/ex069.py
@@ -3,22 +3,29 @@
 # . Quantos homens foram cadastrados.
 # . Quantas mulheres tem menos de 20 anos.
 
-sexo = progresso = ''
-quanthomem = idade = mulher20 = maior18 = 0
+quanthomem = 0
+idade = 0
+mulher20 = 0
+maior18 = 0
 
 while True:
+    sexo = ' '
+    progresso = ' '
     print()
-    sexo = str(input('Informe o sexo da pessoa (M/F): ')).strip().upper()
+    while sexo not in 'MF':
+        sexo = str(input('Informe o sexo da pessoa (M/F): ')).strip().upper()
     idade = int(input('Informe a idade da pessoa: '))
-    progresso = str(input('Deseja continuar? (S/N): ')).strip().upper()
+    while progresso not in 'SN':
+        progresso = str(input('Deseja continuar? (S/N): ')).strip().upper()
     if progresso == 'N':
         break
-    if idade >= 18:
+    elif idade >= 18:
         maior18 += 1
-    if sexo == 'M':
+    elif sexo == 'M':
         quanthomem += 1
-    if (sexo == 'F') and (idade < 20):
-        mulher20 +=1
+    elif sexo == 'F':
+        if idade < 20:
+            mulher20 +=1
 print()
 print(f'Foram registradas {maior18} pessoas maiores de 18 anos, {quanthomem} homens e {mulher20} mulheres menores de 20 anos.')
 print()
