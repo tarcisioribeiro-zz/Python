dd261b7ca3cccd6ed9ea462341f9c15f558736d7
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-15 10:39:33 -0300

    Não sei fazer o exercício 70.

---
+++ Curso-de-Python/Mundo-2/Aula-15/Exercícios/ex070.py
@@ -0,0 +1,14 @@
+# #Ler o preço e nome de vários produtos e perguntar ao usuário se deseja continuar. Ao final, mostar o nome do produto mais barato, quantos produtos custam mais de R$ 1000 e qual o valor total das compras.
+
+# nome = progesso = ''
+# preco = total = maisdemil = 0
+
+# while progesso != 'N':
+#     print()
+#     nome = str(input('Informe o nome do produto: '))
+#     preco = int(input('Informe o valor do produto: '))
+#     progesso = str(input('Deseja continuar? [S/N]: '))
+#     if progesso == 'N':
+#         break
+#     else:
+        
