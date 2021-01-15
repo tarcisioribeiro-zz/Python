bcacb4b6e0f946d97173700aebdcfe7ef6a56e3a
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-15 12:41:49 -0300

    Consegui resolver o exercício 69.

--- Curso-de-Python/Mundo-2/Aula-15/Exercícios/ex069.py
+++ Curso-de-Python/Mundo-2/Aula-15/Exercícios/ex069.py
@@ -8,24 +8,23 @@ idade = 0
 mulher20 = 0
 maior18 = 0
 
-while True:
-    sexo = ' '
-    progresso = ' '
+while True:    
     print()
+    idade = int(input('Informe a idade da pessoa: '))
+    sexo = ' '
     while sexo not in 'MF':
         sexo = str(input('Informe o sexo da pessoa (M/F): ')).strip().upper()
-    idade = int(input('Informe a idade da pessoa: '))
+    if idade >= 18:
+        maior18 += 1
+    if sexo == 'M':
+        quanthomem += 1
+    if sexo == 'F' and idade < 20:
+        mulher20 +=1
+    progresso = ' '
     while progresso not in 'SN':
         progresso = str(input('Deseja continuar? (S/N): ')).strip().upper()
     if progresso == 'N':
         break
-    elif idade >= 18:
-        maior18 += 1
-    elif sexo == 'M':
-        quanthomem += 1
-    elif sexo == 'F':
-        if idade < 20:
-            mulher20 +=1
 print()
-print(f'Foram registradas {maior18} pessoas maiores de 18 anos, {quanthomem} homens e {mulher20} mulheres menores de 20 anos.')
+print(f'{maior18} pessoas maiores de 18 anos foram registradas.\n{quanthomem} homens foram registrados.\n{mulher20} mulheres menores de 20 anos foram registradas.')
 print()
