ba595df75398d1059a427d7a3fb5eec274640441
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-16 19:32:53 -0300

    Tentando consertar o seletor.

--- Games-em-Python/Jogo-do-Carrinho/source/menu/seletor.py
+++ Games-em-Python/Jogo-do-Carrinho/source/menu/seletor.py
@@ -4,15 +4,21 @@ from source.fases.deserto import Deserto
 from source.fases.floresta import Floresta
 from source.fases.praia import Praia
 
+
 def Seletor():
-    fase = int(input(emoji.emojize(
-        'Qual fase deseja jogar? \n\n[ 1 ] Fase da floresta :evergreen_tree:\n[ 2 ] Fase da praia :palm_tree:\n[ 3 ] Fase do deserto :cactus:\n\nDigite aqui sua opção: ', use_aliases=True)))
-    print()
-    sleep(1)
-    if(fase == 1):
-        Floresta()
-    if(fase == 2):
-        Praia()
-    if(fase == 3):
-        Deserto()
+    while True:
+        fase = ' '
+        while fase not in '123SAIR':
+            fase = str(input(emoji.emojize(
+                'Qual fase deseja jogar? \n\n[ 1 ] Fase da floresta :evergreen_tree:\n[ 2 ] Fase da praia :palm_tree:\n[ 3 ] Fase do deserto :cactus:\n\nDigite aqui sua opção: ', use_aliases=True)))
+        print()
+        sleep(1)
+        if fase == 'SAIR':
+            break
+        if fase == '1':
+            Floresta()
+        if fase == '2':
+            Praia()
+        if fase == '3':
+            Deserto()
     pass
