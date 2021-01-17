ed4045acdb5fd6574486ad49d935bdfed2f7e4a3
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-16 22:52:11 -0300

    Jogo feito.

--- Games-em-Python/Roleta-Russa/source/sorteio.py
+++ Games-em-Python/Roleta-Russa/source/sorteio.py
@@ -9,44 +9,49 @@ def Sorteio():
     mixer.music.load('library/sounds/tambor-carregando.mp3')
     mixer.music.play()
     sleep(4)
-    bala = randint(1, 6)
     print(emojize('Revólver carregado, hora do jogo! :ghost: '))
-    while True:
-        gatilho = 0
-        vitória = 0
-        while vitória < 5 or gatilho != bala:
-            gatilho = randint(1, 6)
-            sleep(1)
+    gatilho = 0
+    vitória = 0
+    bala = 0
+    while vitória < 5 or gatilho != bala:
+        bala = randint(1, 6)
+        gatilho = randint(1, 6)
+        sleep(1)
+        print()
+        sleep(1)
+        print('Hora de apertar o gatilho!!!')
+        sleep(1)
+
+        mixer.music.load('library/sounds/tensão.mp3')
+        mixer.music.play()
+        sleep(5)
+
+        if gatilho == bala:
+            mixer.music.load('library/sounds/fogo.mp3')
+            mixer.music.play()
+            sleep(2)
             print()
             sleep(1)
-            print('Hora de apertar o gatilho!!!')
+            print(emojize('Você morreu, otário!!!' +
+                          ' :skull: ', use_aliases=True))
             sleep(1)
-
-            mixer.music.load('library/sounds/tensão.mp3')
+            print()
+            break
+        elif gatilho != bala:
+            mixer.music.load('library/sounds/vazio.mp3')
             mixer.music.play()
-            sleep(5)
-
-            if gatilho == bala:
-                mixer.music.load('library/sounds/fogo.mp3')
-                mixer.music.play()
-                sleep(2)
-                print(emojize('Você morreu, otário!!!' +
-                              ' :skull: ', use_aliases=True))
+            sleep(2)
+            print()
+            sleep(1)
+            print('Dessa vez você escapou!')
+            sleep(1)
+            vitória += 1
+            if vitória == 5:
+                sleep(1)
+                print('Você está solto, pode voltar pra casa.')
                 sleep(1)
                 print()
                 break
-            elif gatilho != bala:
-                mixer.music.load('library/sounds/vazio.mp3')
-                mixer.music.play()
-                sleep(2)
-                print('Dessa vez você escapou!')
-                sleep(1)
-                print()
-                vitória += 1
-                if vitória == 5:
-                    sleep(1)
-                    print('Você está solto, pode voltar pra casa.')
-                    sleep(1)
-                    print()
-                    break
-    pass
+
+
+pass
