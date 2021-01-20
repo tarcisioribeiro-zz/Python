93e57db2bd52e011a2f031f6dcf046a2ed0dfbc8
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-20 18:33:17 -0300

    Fiz a lista de 'Ação'.

---
+++ Solucoes/Solucoes-de-Compras/Locadora/source/index/lista_acao.py
@@ -0,0 +1,18 @@
+from emoji import emojize
+from time import sleep
+from pygame import mixer
+
+def ListaAcao():
+    sleep(1)
+    print()
+    sleep(1)
+    print(emojize('Lista de filmes de Ação :running: :', use_aliases=True))
+    sleep(2)
+    print('O Predador (1987)')
+    sleep(1)
+    print('Rambo 2 (1985)')
+    sleep(1)
+    print('Missão Impossível (1996)')
+    sleep(1)
+    print()
+    pass
