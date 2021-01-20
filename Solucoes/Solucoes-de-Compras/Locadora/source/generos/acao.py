f2c37977fa4c7375f62d652a06e2bbdd8d4c0fe4
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-20 18:51:02 -0300

    Consertei o erro.

--- Solucoes/Solucoes-de-Compras/Locadora/source/generos/acao.py
+++ Solucoes/Solucoes-de-Compras/Locadora/source/generos/acao.py
@@ -7,7 +7,6 @@ from source.index.lista_acao import ListaAcao
 
 
 def Acao():
-    mixer.init()
     ListaAcao()
     sleep(1)
     escolha = str(input('Informe que filme deseja assistir: ')
@@ -19,13 +18,15 @@ def Acao():
         print('Digite uma opção válida.')
         sleep(1)
     if escolha == 'P':
+        mixer.init()
         mixer.music.load('library/sounds/generos/acao/predador.mp3')
         mixer.music.play()
         print(' Dutch é contratado pelo governo dos Estados Unidos para resgatar políticos presos na Guatemala.\nMas quando ele e sua equipe chegam na América Central, logo percebem que há algo errado.\nDepois de descobrir vários cadáveres, a equipe descobre que está sendo caçada por uma criatura brutal\ncom força sobre-humana e uma capacidade surpreendente de se camuflar.')
         sleep(35)
     elif escolha == 'R':
-        mixer.music.load('library/sounds/genero/acao/rambo.mp3')
+        mixer.init()
+        mixer.music.load('library/sounds/generos/acao/rambo.mp3')
         mixer.music.play()
-        print(' Rambo está preso em uma penitenciária federal quando recebe\nnuma proposta: será perdoado e reintegrado ao Exército se\nparticipar de uma missão no Vietnã, onde terá que enfrentar todo\ntipo de inimigos, inclusive oficiais americanos corruptos.')
+        print(' Rambo está preso em uma penitenciária federal quando recebe\numa proposta: será perdoado e reintegrado ao Exército se\nparticipar de uma missão no Vietnã, onde terá que enfrentar todo\ntipo de inimigos, inclusive oficiais americanos corruptos.')
         sleep(58)
     pass
