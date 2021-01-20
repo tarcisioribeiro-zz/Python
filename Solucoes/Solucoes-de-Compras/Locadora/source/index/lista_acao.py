962c3d025ce8117734a21e796c931f69a91077ab
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-20 18:43:09 -0300

    Consertei a lista de 'Ação'.

--- Solucoes/Solucoes-de-Compras/Locadora/source/index/lista_acao.py
+++ Solucoes/Solucoes-de-Compras/Locadora/source/index/lista_acao.py
@@ -4,10 +4,9 @@ from pygame import mixer
 
 def ListaAcao():
     sleep(1)
-    print()
-    sleep(1)
     print(emojize('Lista de filmes de Ação :running: :', use_aliases=True))
     sleep(2)
+    print()
     print('O Predador (1987)')
     sleep(1)
     print('Rambo 2 (1985)')
