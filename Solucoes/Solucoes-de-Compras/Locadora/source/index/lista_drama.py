ae9bba7e3d79b59a75f50c1950d28b2455f8b1c3
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-21 11:40:41 -0300

    Montando a lista 'Drama'.

--- Solucoes/Solucoes-de-Compras/Locadora/source/index/lista_drama.py
+++ Solucoes/Solucoes-de-Compras/Locadora/source/index/lista_drama.py
@@ -1,3 +1,17 @@
 from emoji import emojize
 from time import sleep
-from pygame import mixer
+
+def ListaDrama():
+    sleep(1)
+    print(emojize('Lista de filmes de drama :crying: :', use_aliases=True))
+    sleep(2)
+    print()
+    sleep(1)
+    print(' ()')
+    sleep(1)
+    print(' ()')
+    sleep(1)
+    print(' ()')
+    sleep(1)
+    print()
+    pass
