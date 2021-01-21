d939653b4529438511c9d75780fa07584e842180
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-20 23:22:15 -0300

    Adicionei o Gênero 'Comédia'.

--- Solucoes/Solucoes-de-Compras/Locadora/source/generos/comedia.py
+++ Solucoes/Solucoes-de-Compras/Locadora/source/generos/comedia.py
@@ -1,7 +1,22 @@
+from source.index.lista_comedia import ListaComedia
 from emoji import emojize
 from time import sleep
 from pygame import mixer
 
 def Comedia():
     
+    ListaComedia()
+
+    sleep(1)
+    escolha = str(input('Informe que filme deseja assistir (Digite S para sair): ')
+                  ).strip().upper()[0]
+    sleep(1)
+    print()
+
+    while escolha not in 'PRM':
+
+        sleep(1)
+        print('Digite uma opção válida.')
+        sleep(1)
+        
     pass
--- Solucoes/Solucoes-de-Compras/Locadora/source/index/lista_comedia.py
+++ Solucoes/Solucoes-de-Compras/Locadora/source/index/lista_comedia.py
@@ -9,7 +9,7 @@ def ListaComedia():
     sleep(1)
     print('Pulp Fiction (1994)')
     sleep(1)
-    print('Pequena Miss Sunshine (2006)')
+    print('Little Miss Sunshine (2006)')
     sleep(1)
     print()
     pass
