35bc0f643703b1772bd0f9624ac2a9da3a7b737f
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-20 22:58:02 -0300

    Fiz o index de 'Comédia'.

--- Solucoes/Solucoes-de-Compras/Locadora/source/index/lista_comedia.py
+++ Solucoes/Solucoes-de-Compras/Locadora/source/index/lista_comedia.py
@@ -1,3 +1,15 @@
 from emoji import emojize
 from time import sleep
-from pygame import mixer
+
+def ListaComedia():
+    sleep(1)
+    print(emojize('Lista de filmes de comédia :laughing: :', use_aliases=True))
+    sleep(2)
+    print('Os Caça-Fantasmas (1984)')
+    sleep(1)
+    print('Pulp Fiction (1994)')
+    sleep(1)
+    print('Pequena Miss Sunshine (2006)')
+    sleep(1)
+    print()
+    pass
